447800dc381e2bd124759435f55a88d16d6f7516
JEON-Sungsu <63297236+JEON-Sungsu@users.noreply.github.com>
2021-09-06 08:17:34 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,35 +1,5 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-// search 태그 내에 있는 input을 찾는 효율적인 방법임
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-})
-/*검색 이모지를 클릭하면 인풋을 포커스 시켜라 
-이걸 굳이 왜 사용하냐고 하냐면, input을 focus 시키려면 input을 아주 정확하게
-마우스로 클릭을 해줘야 하는데, input 크기가 작아서 굉장히 불편하다. 해서 input을 전체
-감싸고 있는 부묘오소인 div만 클릭을 해도 자동으로 input에 focus를 줄 수 있기 때문에
-div 요소에다가 click 이라는 동적 요소를 넣는것이다. */
-
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-})
-
-/* 인풋에 포커스가 발생하면 검색 이모지에다가 focused라는 클래스를 추가시키고
- 인풋에는 setAttribute를 통해서 플레이스홀더,통합검색텍스트를 넣어라 */
-
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-})
-
-/*포커스가 해제되면 (blur 처리 되면) 이모지 div에 포커스드 클래스 제거하고
-인풋에다가는 플레이스홀더를 안보이게 없애버려라 */
-
-
-const badgeEl = document.querySelector('header .badges');
-const toTopEl = document.querySelector('#to-top');
+const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 /* window.addEventListener('scroll', function(){
   console.log('scroll!!');
 }) */
@@ -37,39 +7,40 @@ const toTopEl = document.querySelector('#to-top');
  화면 자체에, 스크롤 이벤트가 발생하면 익명의 함수를 실행하겠다. 그런데 스크롤 할때마다
 한번에 너무 많은 횟수를 실행한다. */
 
-window.addEventListener('scroll', _.throttle(function() {
-  console.log(window.scrollY);
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
     //scrollY 를 통해서 콘솔로 스크롤할때마다의 좌표값을 알 수 있다.
-  if (window.scrollY > 500) {
-    //배지 숨기기
-    gsap.to(badgeEl, .6, {
-      opacity: 0,
-      display: 'none'
-    });
-    //버튼보이기
-    gsap.to(toTopEl, 0.2, {
-      x: 0, //x축으로 원래의 자리로 돌리겠다.
-    });
+    if (window.scrollY > 500) {
+      //배지 숨기기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+      //버튼보이기
+      gsap.to(toTopEl, 0.2, {
+        x: 0, //x축으로 원래의 자리로 돌리겠다.
+      });
+    } else {
+      //배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+      //버튼 숨기기
+      gsap.to(toTopEl, 0.2, {
+        x: 100, //x축으로 100픽셀 움직이게 만들겠다.
+      });
+    }
+  }, 300)
+);
 
-  } else {
-    //배지 보이기 
-    gsap.to(badgeEl, .6, {
-      opacity: 1,
-      display: 'block'
-    });
-    //버튼 숨기기
-    gsap.to(toTopEl, 0.2, {
-      x: 100, //x축으로 100픽셀 움직이게 만들겠다.
-    });
-  }
-}, 300));
-
-
- toTopEl.addEventListener('click', function (){
-   gsap.to(window, .7, {
-     scrollTo: 0 //최상단으로 화면을 옮겨라. 이게 gsap의 플러그인을 사용하는 이유임. 
-   });
- });
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0, //최상단으로 화면을 옮겨라. 이게 gsap의 플러그인을 사용하는 이유임.
+  });
+});
 
 /* 300은 0.3초를 의미한다. 로데쉬에서 제공하는 특정한 기능을 통해, 스크롤을 하면 무한대로
 실행되는 함수에다가 0.3초마다 쓰로틀링을 주는 기능이다.
@@ -80,12 +51,11 @@ to(요소, 시간, 옵션) 인데 옵션을 줄 때에는 객체 데이터로 {}
 문자 옵션은 반드시 따옴표 사용할 것. 
 */
 
-
-const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function(fadeEl, index) {
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * 0.7, //0.7, 1.4, 2.1, 2.7
-    opacity: 1
+    opacity: 1,
   });
 });
 
@@ -101,18 +71,17 @@ fadeEls 에 반환시켜라.
 그리하면 첫번째 요소의 인덱스는 0 이므로 0.7 두번째는 1+1*0.7 = 1.4초 세번째는 2+1*0.7 = 2.1초
 이런식으로 각각의 요소에 적용되는 delay가 달라지므로 순서대로 등장하게 된다. */
 
-
-new Swiper('.notice-line .swiper-container', {
-  direction: 'vertical',
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
   autoplay: true,
-  loop: true
+  loop: true,
 });
 
 // new Swiper(선택자,옵션) notice-line 의 swiper-container 를 찾아서 옵션의 내용을 실행시켜라
 // swiper 라이브러리를 사용하는 함수임
 // autoplay = 자동재생, loop= 반복재생여부
 
-new Swiper('.promotion .swiper-container', {
+new Swiper(".promotion .swiper-container", {
   slidesPerView: 3, //한번에 보여줄 슬라이드 개수
   spaceBetween: 10, //슬라이드 사이의 여백
   centeredSlides: true, //1번 슬라이드가 가운데 보이기
@@ -120,44 +89,39 @@ new Swiper('.promotion .swiper-container', {
   /*autoplay: {
     delay: 5000 //0.5초에 한번씩 슬라이드가 넘어감. 기본값은 3초임 
   }*/
-  pagination: { 
-    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
-    clickable: true //사용자의 페이지 번호 요소 제어 버튼
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true, //사용자의 페이지 번호 요소 제어 버튼
   },
   navigation: {
-    prevEl: '.promotion .swiper-prev', // 이전 요소를 볼 수 있는 버튼링크
-    nextEl: '.promotion .swiper-next' // 다음 요소를 볼 수 있는 버튼링크. 
-  }
+    prevEl: ".promotion .swiper-prev", // 이전 요소를 볼 수 있는 버튼링크
+    nextEl: ".promotion .swiper-next", // 다음 요소를 볼 수 있는 버튼링크.
+  },
 });
 
-// direction: horizontal 은 기본옵션임 
+// direction: horizontal 은 기본옵션임
 
-new Swiper('.awards .swiper-container', {
-  autoplay:true,
-  loop:true,
+new Swiper(".awards .swiper-container", {
+  autoplay: true,
+  loop: true,
   spaceBetween: 30,
-  slidesPerView: 5,    //하나의 화면에 몇개의 슬라이드를 보일거냐
+  slidesPerView: 5, //하나의 화면에 몇개의 슬라이드를 보일거냐
   navigation: {
-    prevEl: '.awards .swiper-prev',
-    nextEl: '.awards .swiper-next'
-  }
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
 });
 
-
-
-
-
-
-const promotionEl = document.querySelector('.promotion')
-const promotionToggleBtn = document.querySelector('.toggle-promotion')
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
 let isHidePromotion = false; //isHidePromotion 요소가 숨겨져있니? false 아니!
 
-promotionToggleBtn.addEventListener('click',function() {
-  isHidePromotion = !isHidePromotion //느낌표가 붙어있는 뒤의 값이 반대가 되도록 만들어라
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion; //느낌표가 붙어있는 뒤의 값이 반대가 되도록 만들어라
   if (isHidePromotion) {
-    promotionEl.classList.add('hide');
+    promotionEl.classList.add("hide");
   } else {
-    promotionEl.classList.remove('hide');
+    promotionEl.classList.remove("hide");
   }
 });
 
@@ -168,40 +132,31 @@ if (그값이트루면) 다음동작을 실행시키고 else 그값이 펄스면
 function random(min, max) {
   // `.toFixed()`를 통해 반환된 문자 데이터를,
   // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
-  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
 }
 
-
-
-function floatingObject (selector, delay, size) {
+function floatingObject(selector, delay, size) {
   //gsap.to(요소, 시간, 옵션);
   gsap.to(selector, random(1.5, 2.5), {
     y: size, //Y축으로 얼마나 움직일거냐 floatingObject의 인수를 값으로 받는거임. 아래에 함수호출에 3번째 자리.
-    repeat: -1, //무한반복 지삽에서 제공하는 기능임 
+    repeat: -1, //무한반복 지삽에서 제공하는 기능임
     yoyo: true, //반복한걸 역으로 재생시켜줌. 원래 반보갛면 1에서 10까지 갔다가 다시 1로돌아오는데 10에서 1로 돌아오는 구간을 만들어주는것임
-    duration: 2.5, 
+    duration: 2.5,
     ease: "power1.inOut",
-    delay: random(0, delay) //위에 선언된 함수를 값으로 받고, 그 함수의 매개변수의 최소값은 0이고 최대값은 floatingObject의 두번째 인수인 delay로 받음
+    delay: random(0, delay), //위에 선언된 함수를 값으로 받고, 그 함수의 매개변수의 최소값은 0이고 최대값은 floatingObject의 두번째 인수인 delay로 받음
   });
-
 }
 
-floatingObject('.floating1', 1, 15);
-floatingObject('.floating2', .5, 15);
-floatingObject('.floating3', 1.5, 20);
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
 
-
-const spyEls = document.querySelectorAll('section.scroll-spy');
-spyEls.forEach(function(spyEl) {
-  new ScrollMagic
-    .Scene({
-      triggerElement:spyEl, //보여짐 여부를 감시할 요소를 지정
-      triggerHook: 0.8, //뷰포트의 어떤 지점에서 감시되었는지를 판단하는지 옵션이다. 뷰포트의 제일 상단이 0이고 제일 하단이 1이다.
-    })
-    .setClassToggle(spyEl, 'show')
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8, //뷰포트의 어떤 지점에서 감시되었는지를 판단하는지 옵션이다. 뷰포트의 제일 상단이 0이고 제일 하단이 1이다.
+  })
+    .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller()); //매소드 체이닝
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //매년 자동으로 갱신되는 연도를 삽입
